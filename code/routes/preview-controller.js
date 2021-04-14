d0ac9ee8d92ae7a9466e94a5c11f2becefd35879
Johnny Lin <johnny@openlyoperated.org>
2021-04-14 15:33:48 -0700

    FIX: Handle non-html files in preview

--- code/routes/preview-controller.js
+++ code/routes/preview-controller.js
@@ -7,6 +7,9 @@ const { ValidateCheck } = require("shared/utilities");
 
 // Utilities
 var { getLinkPreview } = require('link-preview-js');
+var axios = require('axios');
+
+const MAX_CONTENT_LENGTH = 2097152 // 2 megabytes is max size we'll preview-fetch
 
 // Routes
 var router = require('express').Router();
@@ -32,23 +35,50 @@ router.post('/preview',
 (request, response, next) => {
   var url = request.values.url;
 
-  return getLinkPreview(url)
-  .then((data) => {
-    // title bug fix - sometimes it's duplicated
-    if (data.title != null && data.title.length % 2 == 0) {
-      let firstHalf = data.title.substring(0, data.title.length/2);
-      let secondHalf = data.title.substring(data.title.length/2, data.title.length);
-      if ( firstHalf == secondHalf ) {
-        data.title = firstHalf
-      }
+  // ensure size is smaller than 5MB or it's not "application" content type
+  let toReturnIfHeadFails = {
+    url: url,
+    title: "",
+    description: ""
+  }
+  axios({
+    method: "HEAD",
+    url: url,
+    timeout: 5000
+  })
+  .then(headResponse => {
+    if (headResponse.headers["content-length"] && parseInt(headResponse.headers["content-length"] > MAX_CONTENT_LENGTH)) {
+      response.send(toReturnIfHeadFails)
     }
-    response.send(data)
+    else if (headResponse.headers["content-type"] && !headResponse.headers["content-type"].startsWith("text/html")) {
+      response.send(toReturnIfHeadFails)
+    }
+    else {
+      return getLinkPreview(url)
+      .then((data) => {
+        // title bug fix - sometimes it's duplicated
+        if (data.title != null && data.title.length % 2 == 0) {
+          let firstHalf = data.title.substring(0, data.title.length/2);
+          let secondHalf = data.title.substring(data.title.length/2, data.title.length);
+          if ( firstHalf == secondHalf ) {
+            data.title = firstHalf
+          }
+        }
+        response.send(data)
+      })
+      .catch( error => {
+        response.status(400).json({
+          message: error
+        });
+      })
+    }
   })
   .catch( error => {
     response.status(400).json({
       message: error
     });
   })
+
 });
 
 module.exports = router;
