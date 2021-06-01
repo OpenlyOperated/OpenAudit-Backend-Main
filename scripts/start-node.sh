82cc3d7829c50f381353c2b3fd1c5f059e932d9b
Johnny Lin <johnny@openlyoperated.org>
2021-06-01 16:38:42 -0700

    Node 12

--- scripts/start-node.sh
+++ scripts/start-node.sh
@@ -5,7 +5,6 @@ set -x
 export NODE_ENV=production
 
 cd /home/node/main
-pm2 update
 env PATH=$PATH:/usr/local/bin /usr/lib/node_modules/pm2/bin/pm2 startup systemd -u node --hp /home/node
 pm2 delete all
 pm2 start bin/www -i max --merge-logs --log ../logs/app.log
--- scripts/update-node.sh
+++ scripts/update-node.sh
@@ -3,11 +3,11 @@
 set -x
 
 npm install -g n
-n 10
+n 12
 npm install -g npm@6
 
 # Install latest pm2
-npm install -g pm2
+npm install pm2@latest -g
 
 # Rotate logs
 pm2 install pm2-logrotate
